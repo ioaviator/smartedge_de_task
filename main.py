8007044f30c4baf907c89189aec9bfcb626bc572
ioaviator <kingsolomonifeanyi@gmail.com>
2025-07-03 11:08:15 +0100

    printing results to console

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 
+from check_data_type import check_data_types
 from check_null import check_nulls
 from config import logger
 from load_data import load_data
@@ -7,7 +8,18 @@ from load_data import load_data
 
 def main(filepath):
   df = load_data(filepath, logger)
-  df = check_nulls(df, logger)
+  nulls = check_nulls(df, logger)
+  data_type = check_data_types(df, logger)
+
+  print("printing result values to console")
+
+  print("\n Null Values:")  
+  print({col: null_count 
+         for col, null_count in nulls.items() })
+
+  print("\n Data Types:")
+  for col, dtype in data_type.items():
+    print(f"{col}: {dtype}")
 
   return None
   
